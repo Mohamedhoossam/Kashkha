e2f8589705a4cac8f29f6c04eac2b4086149b8a9
mohamed hossam omar <1924azaz2555@gmail.com>
2024-09-12 03:18:32 +0300

    Add order end point and add base url before image

---
+++ Kashkha.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkha.DAL
+{
+	public class UnitOfWork : IDisposable, IUnitOfWork
+	{
+		private readonly KashkhaContext _context;
+		public IProductRepository _ProductRepository { get; private set; }
+		public IReviewRepository _reviewRepository { get; private set; }
+		public IOrderRepository _orderRepository { get; private set; }
+		public IOrderItemRepository _orderItemRepository { get; private set; }
+		public ICartRepository _cartRepository { get; private set; }
+
+
+        public UnitOfWork(KashkhaContext context, IConnectionMultiplexer redis)
+		{
+			_ProductRepository = new ProductRepository(context);
+			_reviewRepository = new ReviewRepository(context);
+			_orderRepository = new OrderRepository(context);
+			_orderItemRepository= new OrderItemRepository(context);
+			_cartRepository= new CartRepository(redis);
+            _context = context;
+		}
+
+
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+
+		public int Complete()
+		{
+			return _context.SaveChanges();
+		}
+	}
+}
