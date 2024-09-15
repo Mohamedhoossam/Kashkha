5d2b26a06d889a11da0ae503309d4054cf82dfd4
sa1ah <salahmostafa569@gmail.com>
2024-09-15 23:16:35 +0300

    changing token expiry date

--- Kashkha.DAL/Migrations/20240915195638_auth.Designer.cs
+++ Kashkha.DAL/Migrations/20240915195638_auth.Designer.cs
@@ -4,6 +4,7 @@ using Kashkha.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kashkha.DAL.Migrations
 {
     [DbContext(typeof(KashkhaContext))]
-    partial class KashkhaContextModelSnapshot : ModelSnapshot
+    [Migration("20240915195638_auth")]
+    partial class auth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
